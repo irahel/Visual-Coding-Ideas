90ac2f75ffa6b337b55eb0329e2f17d063d8bf73
Isaac Rahel <isaac_rahel@live.com>
2022-10-05 18:47:13 -0300

    feat: coding resize event

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -132,6 +132,26 @@ const Canvas = (
     };
   }, [draw]);
 
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  React.useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
     <canvas
       ref={canvasRef}
