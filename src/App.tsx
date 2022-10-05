a1fa3a8a758aff3cf3845256907decbc27cfa226
Isaac Rahel <isaac_rahel@live.com>
2022-10-05 11:20:37 -0300

    refactor: componentize btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import Footer from "./components/Footer";
+import Random from "./components/Random";
 import "./index.css";
 
 function App() {
   return (
     <div className="bg-dark w-screen h-screen flex flex-col items-center justify-between">
-      <button
-        className="bg-blue hover:bg-blue-hover rounded-3xl w-24 h-11
-      text-white font-serif font-bold
-      mt-8"
-      >
-        Random
-      </button>
+      <Random />
       <Footer />
     </div>
   );
---
+++ src/components/Random.tsx
@@ -0,0 +1,13 @@
+function Random() {
+  return (
+    <button
+      className="bg-blue hover:bg-blue-hover rounded-3xl w-24 h-11
+      text-white font-serif font-bold
+      mt-8"
+    >
+      Random
+    </button>
+  );
+}
+
+export default Random;
